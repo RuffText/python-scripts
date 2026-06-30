39f02681b3d2deccb6cb4cc959e531d92fedfcdd
RuffText <karki.subastik@gmail.com>
2026-06-30 16:42:20 +0545

    Add budget_app.py

---
+++ learning/budget_app.py
@@ -0,0 +1,37 @@
+class Category:
+    def __init__(self, name):
+        self.name = name
+        self.ledger = []
+
+    def deposit(self, amount, description=""):
+        self.ledger.append({'amount': amount, 'description': description})
+
+    def withdraw(self, amount, description=""):
+        if self.check_funds(amount):
+            self.ledger.append({'amount': -amount, 'description': description})
+            return True
+        else:
+            return False
+
+    def get_balance(self):
+        balance = 0
+        for i in self.ledger:
+            balance += i['amount']
+
+        return balance
+
+    def transfer(self, amount, category):
+        if self.check_funds(amount):
+            self.withdraw(amount, f"Transfer to {category.name}")
+            category.deposit(amount, f"Transfer from {self.name}")
+            return True
+        else:
+            return False
+
+    def check_funds(self, amount):
+        return True if amount < self.get_balance() else False
+
+    def __str__(self):
+        result = ""
+        result
+
