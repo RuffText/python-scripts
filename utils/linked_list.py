cdb0707081da33fe5f6a429aa5f5d49f50f21bac
RuffText <karki.subastik@gmail.com>
2026-07-20 20:15:49 +0545

    Update linked_list.py

--- utils/linked_list.py
+++ utils/linked_list.py
@@ -1,47 +1,129 @@
 class LinkedList:
     class Node:
-        def __init__(self, element):
-            self.element = element
+        def __init__(self, value):
+            self.value = value
             self.next = None
-            
+
     def __init__(self):
-        self.length = 0
         self.head = None
 
-    def is_empty(self):
-        return self.length == 0
-    
-    def add(self, element):
-        node = self.Node(element)
-        if self.is_empty():
-            self.head = node
-        else:
-            current_node = self.head
-            while current_node.next is not None:
-                current_node = current_node.next
-            current_node.next = node
-        self.length += 1
+    def __repr__(self):
+        if self.head is None:
+            return "[]"
+        nodes = []
+        current = self.head
+        while current:
+            nodes.append(repr(current.value))
+            current = current.next
+        return "[" + ", ".join(nodes) + "]"
 
-    def remove(self, element):
-        previous_node = None
-        current_node = self.head
-        while current_node is not None and current_node.element != element:
-            previous_node = current_node
-            current_node = current_node.next
-        if current_node is None:
-            return        
-        elif previous_node is not None:
-            previous_node.next = current_node.next
+    def __contains__(self, value):
+        current = self.head
+        while current is not None:
+            if current.value == value:
+                return True
+            current = current.next
+        return False
+
+    def __len__(self):
+        current = self.head
+        counter = 0
+        while current is not None:
+            counter += 1
+            current = current.next
+        return counter
+    
+    def append(self, value):
+        if self.head is None:
+            self.head = self.Node(value)
         else:
-            self.head = current_node.next
-        self.length -= 1
+            last = self.head
+            while last.next:
+                last = last.next
+            last.next = self.Node(value)
+
+    def prepend(self, value):
+        first_node = self.Node(value)
+        first_node.next = self.head
+        self.head = first_node
+
+    def insert(self, index, value):
+        if index < 0:
+            raise IndexError("Index out of bounds")
+        if index == 0:
+            self.prepend(value)
+            return
+        
+        if self.head is None:
+            raise IndexError("Index out of bounds")
+            
+        last = self.head
+        for i in range(index - 1):
+            if last.next is None:
+                raise IndexError("Index out of bounds")
+            last = last.next
+
+        new_node = self.Node(value)
+        new_node.next = last.next
+        last.next = new_node
+
+    def delete(self, value):
+        if self.head is None:
+            return
+        if self.head.value == value:
+            self.head = self.head.next
+            return
+            
+        last = self.head
+        while last.next:
+            if last.next.value == value:
+                last.next = last.next.next
+                return
+            last = last.next
+
+    def pop(self, index):
+        if self.head is None or index < 0:
+            raise IndexError("Index out of bounds")
+            
+        if index == 0:
+            popped_value = self.head.value
+            self.head = self.head.next
+            return popped_value
+
+        last = self.head
+        for i in range(index - 1):
+            if last.next is None:
+                raise IndexError("Index out of bounds")
+            last = last.next
+
+        if last.next is None:
+            raise IndexError("Index out of bounds")
+            
+        popped_value = last.next.value
+        last.next = last.next.next
+        return popped_value
+
+    def get(self, index):
+        if self.head is None or index < 0:
+            raise IndexError("Index out of bounds")
+            
+        current = self.head
+        for i in range(index):
+            current = current.next
+            if current is None:
+                raise IndexError("Index out of bounds")
+        return current.value
+
+    def __str__(self):
+        if self.head is None:
+            return "Empty List"
+        nodes = []
+        current = self.head
+        while current:
+            nodes.append(str(current.value))
+            current = current.next
+        nodes.append("None")
+        return " -> ".join(nodes)
 
 if __name__ == "__main__":
-    my_list = LinkedList()
-    my_list.add(1)
-    my_list.add(2)
-    my_list.add(3)
-    my_list.add(4)
-    my_list.add(5)
-    for i in range(my_list.length):
-        print(my_list.Node(i+1).element)
+    pass
